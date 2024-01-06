be3962f245c167bcebd60728a1f40787700f5390
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2024-01-06 19:19:00 -0300

    adding the document ai google api to extract the raw text from file

--- app.js
+++ app.js
@@ -5,13 +5,33 @@ const cors = require('cors');
 app.disable('x-powered-by');
 app.use(cors());
 const port = process.env.PORT || 3000;
+const fs = require('fs').promises;
 
 const upload = require('./upload/upload');
-
 const fileTextExtractorMiddleware = require('./util/fileTextExtractorMiddleware');
 
+const { DocumentProcessorServiceClient } = require('@google-cloud/documentai').v1;
+const client = new DocumentProcessorServiceClient();
+
 app.post('/file-text-extractor', upload.single('file'), fileTextExtractorMiddleware, async (req, res) => {
-    res.send("Pronto: " + req.file.path + "\n Data to extract: " + req.body.data);
+    const path = req.file.path;
+    const data = req.body.data;
+    const mimetype = req.file.mimetype;
+
+    const file = await fs.readFile(path);
+    const encodedFile = file.toString('base64');
+
+    const request = {
+        name: client.processorPath(process.env.PROJECT_ID, "us", process.env.PROCESSOR_ID),
+        rawDocument: {
+            content: encodedFile,
+            mimeType: mimetype,
+        },
+    };
+
+    const [result] = await client.processDocument(request);
+    const { text } = result.document;
+    res.json({ text: text });
 });
 
 app.listen(port, () => {
