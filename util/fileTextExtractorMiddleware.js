c3220c01f043736cba231e8b75b5ae438be1023c
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2024-01-10 18:31:30 -0300

    implementing improvements

--- util/fileTextExtractorMiddleware.js
+++ util/fileTextExtractorMiddleware.js
@@ -2,22 +2,24 @@ const validMimeTypes = ["application/pdf", "image/gif", "image/tiff", "image/jpe
 
 const fileTextExtractorMiddleware = (req, res, next) => {
     if (!req.file) {
-        res.status(400).send('No file uploaded.');
+        res.status(400).send({ error: 'No file uploaded.' });
         return;
     }
+
     if (!validMimeTypes.includes(req.file.mimetype)) {
-        res.status(400).send('Invalid file type.');
+        res.status(400).send({ error: 'Invalid file type.' });
         return;
     }
-    if (req.body.data) {
-        try {
-            JSON.parse(req.body.data);
-        } catch (e) {
-            res.status(400).send('Invalid data to extract.');
-            return;
-        }
-    } else {
-        res.status(400).send('No data to extract.');
+
+    if (!req.body.data) {
+        res.status(400).send({ error: 'No data to extract.' });
+        return;
+    }
+    
+    try {
+        JSON.parse(req.body.data);
+    } catch (e) {
+        res.status(400).send({ error: 'Invalid data to extract.' });
         return;
     }
 
